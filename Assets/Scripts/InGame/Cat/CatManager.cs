5787c90b8a07a1f92806ce76b0a7ca7d207b7f81
Black_Penguin <dkstmdwnswns@gmail.com>
2022-11-07 03:42:51 +0900

    Bug Fix

--- Assets/Scripts/InGame/Cat/CatManager.cs
+++ Assets/Scripts/InGame/Cat/CatManager.cs
@@ -2,14 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CatInfo", menuName = "CatInfo", order = int.MinValue)]
-[System.Serializable]
-public class CatInfo : ScriptableObject
-{
-    public string CatName;
-    public Sprite CatSprite;
-    public CatSkinType CatType;
-}
 public class CatManager : Singleton<CatManager>
 {
     public List<CatData> CatList = new List<CatData>();
